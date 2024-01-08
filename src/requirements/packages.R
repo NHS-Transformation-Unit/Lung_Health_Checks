4061136771328f01adecaf7ad841cd39f08e71c8
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-08 17:09:29 +0000

    Add initial packages

---
+++ src/requirements/packages.R
@@ -0,0 +1,8 @@
+packages <- c("here",
+              "tidyverse",
+              "readxl",
+              "htmltools",
+              "knitr",
+              "kableExtra")
+
+lapply(packages, library, character.only=TRUE)
