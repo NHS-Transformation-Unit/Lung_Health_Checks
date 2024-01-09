e2cbc3e8c070e1ed559b64ad73ef3a6a1eb7d171
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-09 12:08:57 +0000

    Create initial treatment assumptions config

---
+++ src/config/assumptions_treatments_initial.R
@@ -0,0 +1,33 @@
+
+# Initial Cancer Findings -------------------------------------------------
+
+initial_cancer_rate <- 0.508
+initial_m3_fu_rate <- 1 - initial_cancer_rate
+
+# Initial Treatment Rates -------------------------------------------------
+
+initial_treatment_df <- data.frame(
+  Modality = c("Surgery",
+               "Surgery and Adjuvant Chemotherapy",
+               "SABR",
+               "XRT",
+               "Chemo-Radiation",
+               "Chemotherapy",
+               "No Treatment or BSC"),
+  Rates = c(0.510,
+            0.077,
+            0.122,
+            0.091,
+            0.091,
+            0.046,
+            NA)
+) %>%
+  mutate(Rates = case_when(Rates != "No Treatment or BSC" ~ Rates,
+                           TRUE ~ 1- sum(Rates, na.rm = TRUE))) %>%
+  mutate(Modality = factor(Modality, levels = c("Surgery",
+                                                "Surgery and Adjuvant Chemotherapy",
+                                                "SABR",
+                                                "XRT",
+                                                "Chemo-Radiation",
+                                                "Chemotherapy",
+                                                "No Treatment or BSC")))
