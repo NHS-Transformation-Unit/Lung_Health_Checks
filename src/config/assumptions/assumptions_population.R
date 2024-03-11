2b1f24b14a39809e6080a9b35a1355c9e0608fd5
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-11 18:54:17 +0000

    Update population assumptions for multi-year model

--- src/config/assumptions/assumptions_population.R
+++ src/config/assumptions/assumptions_population.R
@@ -5,8 +5,37 @@ total_pop <- assumptions_pop[[1,4]]
 
 # Smoking Rate ------------------------------------------------------------
 
-smk_rate <- assumptions_pop[[2,4]]
+smk_rate_current_y1 <- assumptions_pop[[2,4]]
+smk_rate_current_y2 <- assumptions_pop[[2,5]]
+smk_rate_current_y3 <- assumptions_pop[[2,6]]
+smk_rate_current_y4 <- assumptions_pop[[2,7]]
+smk_rate_current_y5 <- assumptions_pop[[2,8]]
+
+smk_rate_prev_y1 <- assumptions_pop[[3,4]]
+smk_rate_prev_y2 <- assumptions_pop[[3,5]]
+smk_rate_prev_y3 <- assumptions_pop[[3,6]]
+smk_rate_prev_y4 <- assumptions_pop[[3,7]]
+smk_rate_prev_y5 <- assumptions_pop[[3,8]]
+
 
 # Uptake Rate -------------------------------------------------------------
 
-uptake_rate <- assumptions_pop[[3,4]]
+uptake_rate_y1 <- assumptions_pop[[4,4]]
+uptake_rate_y2 <- assumptions_pop[[4,5]]
+uptake_rate_y3 <- assumptions_pop[[4,6]]
+uptake_rate_y4 <- assumptions_pop[[4,7]]
+uptake_rate_y5 <- assumptions_pop[[4,8]]
+
+# Age in and out ----------------------------------------------------------
+
+age_in_y1 <- assumptions_pop[[5,4]]
+age_in_y2 <- assumptions_pop[[5,5]]
+age_in_y3 <- assumptions_pop[[5,6]]
+age_in_y4 <- assumptions_pop[[5,7]]
+age_in_y5 <- assumptions_pop[[5,8]]
+
+age_out_y1 <- assumptions_pop[[6,4]]
+age_out_y2 <- assumptions_pop[[6,5]]
+age_out_y3 <- assumptions_pop[[6,6]]
+age_out_y4 <- assumptions_pop[[6,7]]
+age_out_y5 <- assumptions_pop[[6,8]]
