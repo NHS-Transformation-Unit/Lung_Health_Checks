4c3d8e6d4b79ff37e96b408833c71f3f4bc9b20f
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-11 20:53:36 +0000

    Create script for Year 1 population simulation

---
+++ src/model/y1_1_pop.R
@@ -0,0 +1,11 @@
+
+# Simulate Year 1 Uptake Population ---------------------------------------
+
+y1_uptake_pop <- uptake_pop(start_pop = total_pop,
+                            age_in = age_in_y1,
+                            age_out = age_out_y1,
+                            smk_current = smk_rate_current_y1,
+                            smk_prev = smk_rate_prev_y1,
+                            screened_prev = 0,
+                            repeats = 0,
+                            uptake = uptake_rate_y1)
