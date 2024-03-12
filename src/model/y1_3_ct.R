a13fd2f6cfefe9e512edb42efd9bc3afa931682c
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-12 14:44:56 +0000

    Add CT scan positive dataframe for next stage

--- src/model/y1_3_ct.R
+++ src/model/y1_3_ct.R
@@ -7,3 +7,5 @@ y1_ct_list <- ct(input_df = y1_lhc_pos,
            dna_rebook_rate = ct_dna_rebook_rate_y1,
            positive = ct_scan_positive_y1,
            negative_fu = ct_scan_negative_24m_fu_y1)
+
+y1_ct_output = y1_ct_list[[5]]
