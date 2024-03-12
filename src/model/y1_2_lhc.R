adc0bcecb3f5e7dba36442617a1eb53a865c1282
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-12 11:12:21 +0000

    Add extraction from list outputs for next stage

--- src/model/y1_2_lhc.R
+++ src/model/y1_2_lhc.R
@@ -7,3 +7,5 @@ y1_lhc_list <- lhc(uptake_pop = y1_uptake_pop,
                    lhc_dna_rebook_rate = lhc_dna_rebooked_rate_y1,
                    positive = lhc_positive_rate_y1,
                    negative_ri = lhc_negative_ri_rate_y1)
+
+y1_lhc_pos <- y1_lhc_list[[4]]
