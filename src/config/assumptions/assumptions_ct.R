93e7ed1078f01ae2b3c4c042c5b5ea43572d54cd
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-12 10:07:02 +0000

    Update CT assumptions for multi-year modelling

--- src/config/assumptions/assumptions_ct.R
+++ src/config/assumptions/assumptions_ct.R
@@ -1,20 +1,44 @@
 
 # CT Scan Attendance --------------------------------------------------------
 
-ct_exclusion_rate <- assumptions_ct[[1,4]]
-ct_dna_rate <- assumptions_ct[[2,4]]
-ct_attend_rate <- 1 - ct_exclusion_rate - ct_dna_rate
+ct_exclusion_rate_y1 <- assumptions_ct[[1,4]]
+ct_exclusion_rate_y2 <- assumptions_ct[[1,5]]
+ct_exclusion_rate_y3 <- assumptions_ct[[1,6]]
+ct_exclusion_rate_y4 <- assumptions_ct[[1,7]]
+ct_exclusion_rate_y5 <- assumptions_ct[[1,8]]
+
+# DNA ---------------------------------------------------------------------
+
+ct_dna_rate_y1 <- assumptions_ct[[2,4]]
+ct_dna_rate_y2 <- assumptions_ct[[2,5]]
+ct_dna_rate_y3 <- assumptions_ct[[2,6]]
+ct_dna_rate_y4 <- assumptions_ct[[2,7]]
+ct_dna_rate_y5 <- assumptions_ct[[2,8]]
 
 # DNA Rebooking -----------------------------------------------------------
 
-ct_dna_rebook_rate <- assumptions_ct[[3,4]]
-
-# Indeterminate CT Scan Rate ----------------------------------------------
-
-ct_indeterminate_rate <- assumptions_ct[[4,4]]
+ct_dna_rebook_rate_y1 <- assumptions_ct[[3,4]]
+ct_dna_rebook_rate_y2 <- assumptions_ct[[3,5]]
+ct_dna_rebook_rate_y3 <- assumptions_ct[[3,6]]
+ct_dna_rebook_rate_y4 <- assumptions_ct[[3,7]]
+ct_dna_rebook_rate_y5 <- assumptions_ct[[3,8]]
 
 # CT Scan Findings --------------------------------------------------------
 
-ct_scan_positive <- assumptions_ct[[5,4]]
-ct_scan_negative <- 1 - ct_scan_positive
-ct_scan_negative_24m_fu <- assumptions_ct[[6,4]]
+ct_scan_positive_y1 <- assumptions_ct[[4,4]]
+ct_scan_positive_y2 <- assumptions_ct[[4,5]]
+ct_scan_positive_y3 <- assumptions_ct[[4,6]]
+ct_scan_positive_y4 <- assumptions_ct[[4,7]]
+ct_scan_positive_y5 <- assumptions_ct[[4,8]]
+
+ct_scan_negative_y1 <- 1 - ct_scan_positive_y1
+ct_scan_negative_y2 <- 1 - ct_scan_positive_y2
+ct_scan_negative_y3 <- 1 - ct_scan_positive_y3
+ct_scan_negative_y4 <- 1 - ct_scan_positive_y4
+ct_scan_negative_y5 <- 1 - ct_scan_positive_y5
+
+ct_scan_negative_24m_fu_y1 <- assumptions_ct[[5,4]]
+ct_scan_negative_24m_fu_y2 <- assumptions_ct[[5,5]]
+ct_scan_negative_24m_fu_y3 <- assumptions_ct[[5,6]]
+ct_scan_negative_24m_fu_y4 <- assumptions_ct[[5,7]]
+ct_scan_negative_24m_fu_y5 <- assumptions_ct[[5,8]]
