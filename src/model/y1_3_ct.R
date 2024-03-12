45999bfb116609d4f3600c6a973d9f4858d3ea3d
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-12 11:13:00 +0000

    Add Year 1 CT scan model run

---
+++ src/model/y1_3_ct.R
@@ -0,0 +1,9 @@
+
+# Simulate Year 1 CT Scan Outcomes ----------------------------------------
+
+y1_ct_list <- ct(input_df = y1_lhc_pos,
+           exclusion_rate = ct_exclusion_rate_y1,
+           dna_rate = ct_dna_rate_y1,
+           dna_rebook_rate = ct_dna_rebook_rate_y1,
+           positive = ct_scan_positive_y1,
+           negative_fu = ct_scan_negative_24m_fu_y1)
