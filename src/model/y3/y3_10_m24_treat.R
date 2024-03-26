7b589b0cea632a399bb73770624aac1a7acabec0
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-26 15:54:15 +0000

    Create year 3 repeats dataframes

--- src/model/y3/y3_10_m24_treat.R
+++ src/model/y3/y3_10_m24_treat.R
@@ -11,7 +11,9 @@ y3_m24_treat_groups_list <- m24_treat_groups(input_df = y3_m24_FU_input_df,
 
 y3_m24_add_diags_df <- y3_m24_treat_groups_list[[3]]
 y3_m24_cancer_df <- y3_m24_treat_groups_list[[5]]
-
+y3_m24_opt_out <- y3_m24_treat_groups_list[[7]] %>%
+  select(1:3)
+y3_m48_FU_input_df <- y3_m24_treat_groups_list[[4]]
 
 # Simulate Year 3 24-month Treatment Modalities ----------------------------
 
@@ -23,25 +25,3 @@ y3_m24_modalities_df <- m24_treatment_modalities(input_df = y3_m24_cancer_df,
                                                  chemrad_rate = m24_treatment_chemrad_y3,
                                                  chemo_rate = m24_treatment_chemo_y3,
                                                  nadj_imm_rate = m24_treatment_nadj_imm_y3)
-
-y3_m48_FU_input_df <- y3_m24_treat_groups_list[[4]]
-
-# Creating outputs to inform m24 reinvites
-
-y3_m24_opt_out <- y3_m24_treat_groups_list[[7]] %>%
-  select(1:3)
-
-# Binding all m24 reinvite outputs together
-
-y5_repeat_df <-bind_rows(y3_lhc_opt_out,
-                         y3_lhc_dna_opt_out,
-                         y3_lhc_neg_ri,
-                         y3_ct_opt_out,
-                         y3_m3_opt_out,
-                         y3_m12_opt_out,
-                         y3_m24_opt_out) # Do some of these need to be taken out?
-
-y5_repeat_df <- y5_repeat_df %>%
-  summarize(total_average = n() / length(unique(Trial)))
-
-y5_repeat_input <- y5_repeat_df[1, 1]
