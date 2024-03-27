8139ecca29b15b022667d61d16f6ac9844558163
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-03-27 17:15:14 +0000

    changes to ensure that table values are rounded to 0 dp and display appropriate separators.

---
+++ src/visualisation/tables/tables_run.R
@@ -0,0 +1,3 @@
+
+# Run tables files for each year
+source(paste0(here(),"/src/visualisation/tables/y1_tables.R"))
--- src/visualisation/tables/y1_tables.R
+++ src/visualisation/tables/y1_tables.R
@@ -1,24 +1,27 @@
 
 # Y1 summary table creation
 
-## Uptake population table
+library(scales)
+
+## Uptake Population Table -------------------------------------------------
 
 ### Creating input df
 
 y1_uptake_pop_tab <- y2_screened_input_df %>%
   summarize(
-    percentile_10 = quantile(Total, probs = 0.1, na.rm = TRUE),
-    lower_quartile = quantile(Total, probs = 0.25, na.rm = TRUE),
-    median = median(Total, na.rm = TRUE),
-    upper_quartile = quantile(Total, probs = 0.75, na.rm = TRUE),
-    percentile_90 = quantile(Total, probs = 0.9, na.rm = TRUE)
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
   )
 
-print(y1_uptake_pop_tab)
+y1_uptake_pop_tab_formatted <- y1_uptake_pop_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
 
 ### Creating table output
 
-y1_uptake_pop_tab_vis <- y1_uptake_pop_tab %>%
+y1_uptake_pop_tab_vis <- y1_uptake_pop_tab_formatted %>%
   ungroup() %>%
   rename("10th Percentile" = 1,
          "Lower Quartile" = 2,
@@ -27,5 +30,36 @@ y1_uptake_pop_tab_vis <- y1_uptake_pop_tab %>%
          "90th Percentile" = 5) %>%
   kable(format = "html", align = "lrrrrr") %>%
   kable_styling() %>%
-  row_spec(0, background = "#337AB7", color = "white")
+  row_spec(0, background = "#407EC9", color = "white")
+
+## lhc activity table ------------------------------------------------------
+
+### Creating input df
+
+y1_lhc_activity_tab <- y1_lhc_list[[3]] %>%
+  group_by(lhc_initial_dna_rebook) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
+
+y1_lhc_activity_tab_formatted <- y1_lhc_activity_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_lhc_activity_tab_vis <- y1_lhc_activity_tab_formatted %>%
+  ungroup() %>%
+  rename("Attendance Outcome" = 1,
+         "10th Percentile" = 2,
+         "Lower Quartile" = 3,
+         "Median" = 4,
+         "Upper Quartile" = 5,
+         "90th Percentile" = 6) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
 
