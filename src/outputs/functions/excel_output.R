07e125284221d61360ae62f94ef7b5e6aa8fb18f
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-03-27 13:52:25 +0000

    Further changes to both the output function and the Y1 script, ready to replicate across all years.

--- src/outputs/functions/excel_output.R
+++ src/outputs/functions/excel_output.R
@@ -1,17 +1,18 @@
 
 ## Creating df output function
 
-excel_output <- function(file_path, ...) {
+excel_output <- function(file_path, df_list, overwrite = FALSE) {
   wb <- createWorkbook()
   
-  for (i in 1:length(list(...))) {
-    df_name <- names(list(...))[i]
-    df <- list(...)[[i]]
+
+  for (i in seq_along(df_list)) {
+    df_name <- names(df_list)[i]
+    df <- df_list[[i]]
     
     addWorksheet(wb, sheetName = df_name)
     
     writeData(wb, sheet = df_name, x = df)
   }
   
-  saveWorkbook(wb, file_path)
+  saveWorkbook(wb, file_path, overwrite = overwrite)
 }
