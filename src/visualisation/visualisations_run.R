4482d8a793e5a3d26ebd3055715197249f42f947
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-27 10:31:10 +0000

    Create visualisations run script

---
+++ src/visualisation/visualisations_run.R
@@ -0,0 +1,13 @@
+
+# Source Themes file
+source(paste0(here(), "/src/config/tu_ggplot_themes.R"))
+
+# Source visualisation functions
+source(paste0(here(), "/src/visualisation/functions/uptake_population.R"))
+
+# Run visualisation files for each year
+source(paste0(here(),"/src/visualisation/y1_visuals.R"))
+source(paste0(here(),"/src/visualisation/y2_visuals.R"))
+source(paste0(here(),"/src/visualisation/y3_visuals.R"))
+source(paste0(here(),"/src/visualisation/y4_visuals.R"))
+source(paste0(here(),"/src/visualisation/y5_visuals.R"))
