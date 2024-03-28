43788b48f5c97aefdd0fe6f789c5b80599c87d5e
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-03-28 12:38:16 +0000

    Further work on init treat/diag tables.

--- src/visualisation/tables/y1_tables.R
+++ src/visualisation/tables/y1_tables.R
@@ -287,8 +287,142 @@ y1_ct_neg_fu_tab_vis <- y1_ct_neg_fu_tab_formatted %>%
 
 # ct scan opt-outs --------------------------------------------------------
 
+y1_ct_opt_out_tab <- y1_ct_list[[2]] %>%
+  ungroup() %>%
+  complete(Trial, ct_rebook, fill = list(Total = 0)) %>%
+  filter(ct_rebook == "Opt-out") %>%
+  group_by(ct_rebook) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
+
+y1_ct_opt_out_tab_formatted <- y1_ct_opt_out_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_ct_opt_out_tab_vis <- y1_ct_opt_out_tab_formatted %>%
+  select(-1) %>%
+  ungroup() %>%
+  rename("10th Percentile" = 1,
+         "Lower Quartile" = 2,
+         "Median" = 3,
+         "Upper Quartile" = 4,
+         "90th Percentile" = 5) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
 
 
 # init scan outcomes table ------------------------------------------------
 
+y1_ct_init_scan_tab <- y1_init_treat_groups_list[[1]] %>%
+  ungroup() %>%
+  complete(Trial, init_treat_outcome, fill = list(Total = 0)) %>%
+  mutate(init_treat_outcome = ifelse(init_treat_outcome == "3M_FU", "3M follow-up", init_treat_outcome)) %>%
+  group_by(init_treat_outcome) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
 
+y1_ct_init_scan_tab_formatted <- y1_ct_init_scan_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_ct_init_scan_tab_vis <- y1_ct_init_scan_tab_formatted %>%
+  ungroup() %>%
+  rename("CT Scan Outcome" = 1,
+         "10th Percentile" = 2,
+         "Lower Quartile" = 3,
+         "Median" = 4,
+         "Upper Quartile" = 5,
+         "90th Percentile" = 6) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
+
+
+# init diag mod table -----------------------------------------------------
+
+y1_init_diags_mod_tab_df <- pivot_longer(y1_init_diags_modalities_df, 
+                                         cols = c(pet, bronch, ebus, ct_bio, nav_bronch, lft),
+                                         names_to = "Modality",
+                                         values_to = "Total")
+
+y1_init_diags_mod_tab <- y1_init_diags_mod_tab_df %>%
+  ungroup() %>%
+  complete(Trial, Modality, fill = list(Total = 0)) %>%
+  mutate(Modality = case_when(
+    Modality == "pet" ~ "PET CT",
+    Modality == "bronch" ~ "Bronchoscopy",
+    Modality == "ebus" ~ "EBUS",
+    Modality == "ct_bio" ~ "CT guided biopsy",
+    Modality == "nav_bronch" ~ "Navigation bronchoscopy",
+    Modality == "lft" ~ "Lung function test",
+    TRUE ~ Modality)) %>%
+  group_by(Modality) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
+
+y1_init_diags_mod_tab_formatted <- y1_init_diags_mod_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_init_diags_mod_tab_vis <- y1_init_diags_mod_tab_formatted %>%
+  ungroup() %>%
+  rename("Diagnostic Modality" = 1,
+         "10th Percentile" = 2,
+         "Lower Quartile" = 3,
+         "Median" = 4,
+         "Upper Quartile" = 5,
+         "90th Percentile" = 6) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
+
+
+# init diag malignancy table ----------------------------------------------
+
+y1_init_outcomes_tab <- y1_init_treat_groups_list[[4]] %>%
+  ungroup() %>%
+  complete(Trial, cancer_outcome, fill = list(Total = 0)) %>%
+  group_by(cancer_outcome) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
+
+y1_init_outcomes_tab_formatted <- y1_init_outcomes_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_init_outcomes_tab_formatted <- y1_init_outcomes_tab_formatted %>%
+  ungroup() %>%
+  rename("Cancer Diagnosis Outcome" = 1,
+         "10th Percentile" = 2,
+         "Lower Quartile" = 3,
+         "Median" = 4,
+         "Upper Quartile" = 5,
+         "90th Percentile" = 6) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
