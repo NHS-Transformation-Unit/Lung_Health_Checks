01f35320d2552eebec0afc76a63bbc53736cbf76
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-28 15:14:03 +0000

    Create summary tables

--- src/visualisation/tables/tables_run.R
+++ src/visualisation/tables/tables_run.R
@@ -1,7 +1,8 @@
 
-# Run tables files for each year
+# Run tables files for each year and summary
 source(paste0(here(),"/src/visualisation/tables/y1_tables.R"))
 source(paste0(here(),"/src/visualisation/tables/y2_tables.R"))
 source(paste0(here(),"/src/visualisation/tables/y3_tables.R"))
 source(paste0(here(),"/src/visualisation/tables/y4_tables.R"))
 source(paste0(here(),"/src/visualisation/tables/y5_tables.R"))
+source(paste0(here(),"/src/visualisation/tables/summary_tables.R"))
