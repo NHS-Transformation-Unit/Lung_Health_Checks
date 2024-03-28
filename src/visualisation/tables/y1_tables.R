5862778a23a1e68c5f197dc50dd844f9554bd3cf
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-03-28 11:40:06 +0000

    Added in 0 trial error fix and further developed CT scanning tables.

--- src/visualisation/tables/y1_tables.R
+++ src/visualisation/tables/y1_tables.R
@@ -7,6 +7,12 @@ library(scales)
 
 ### Creating input df
 
+trial_df <- data.frame("Trial" = seq(1:trials)) ## for all inputs that only include activity 
+
+y2_screened_input_df <- trial_df %>% ## 
+  left_join(y2_screened_input_df, by = c("Trial")) %>% ## 
+  mutate(Total = replace_na(Total, 0)) ## 
+
 y1_uptake_pop_tab <- y2_screened_input_df %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -38,6 +44,8 @@ y1_uptake_pop_tab_vis <- y1_uptake_pop_tab_formatted %>%
 ### Creating input df
 
 y1_lhc_activity_tab <- y1_lhc_list[[3]] %>%
+  ungroup() %>%
+  complete(Trial, lhc_initial_dna_rebook, fill = list(Total = 0)) %>%
   group_by(lhc_initial_dna_rebook) %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -70,6 +78,8 @@ y1_lhc_activity_tab_vis <- y1_lhc_activity_tab_formatted %>%
 ### Creating input df
 
 y1_lhc_outcome_tab <- y1_lhc_list[[4]] %>%
+  ungroup() %>%
+  complete(Trial, lhc_outcome, fill = list(Total = 0)) %>%
   group_by(lhc_outcome) %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -108,6 +118,8 @@ y1_lhc_opt_out_tab <- y1_lhc_opt_out_tab %>%
   summarise("Total" = n())
 
 y1_lhc_opt_out_tab <- y1_lhc_opt_out_tab %>%
+  ungroup() %>%
+  complete(Trial, lhc_initial_dna_rebook, fill = list(Total = 0)) %>%
   group_by(lhc_initial_dna_rebook) %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -123,13 +135,13 @@ y1_lhc_opt_out_tab_formatted <- y1_lhc_opt_out_tab %>%
 ### Creating table output
 
 y1_lhc_opt_out_tab_vis <- y1_lhc_opt_out_tab_formatted %>%
+  select(-1) %>%
   ungroup() %>%
-  rename("LHC Outcome" = 1,
-         "10th Percentile" = 2,
-         "Lower Quartile" = 3,
-         "Median" = 4,
-         "Upper Quartile" = 5,
-         "90th Percentile" = 6) %>%
+  rename("10th Percentile" = 1,
+         "Lower Quartile" = 2,
+         "Median" = 3,
+         "Upper Quartile" = 4,
+         "90th Percentile" = 5) %>%
   kable(format = "html", align = "lrrrrr") %>%
   kable_styling() %>%
   row_spec(0, background = "#407EC9", color = "white")
@@ -140,6 +152,8 @@ y1_lhc_opt_out_tab_vis <- y1_lhc_opt_out_tab_formatted %>%
 ### Creating input df
 
 y1_lhc_neg_ri_tab <- y1_lhc_list[[5]] %>%
+  ungroup() %>%
+  complete(Trial, lhc_negative_ri, fill = list(Total = 0)) %>%
   filter(lhc_negative_ri == "Invite") %>%
   group_by(lhc_negative_ri) %>%
   summarize(
@@ -156,13 +170,13 @@ y1_lhc_neg_ri_tab_formatted <- y1_lhc_neg_ri_tab %>%
 ### Creating table output
 
 y1_lhc_neg_ri_tab_vis <- y1_lhc_neg_ri_tab_formatted %>%
+  select(-1) %>%
   ungroup() %>%
-  rename("LHC Outcome" = 1,
-         "10th Percentile" = 2,
-         "Lower Quartile" = 3,
-         "Median" = 4,
-         "Upper Quartile" = 5,
-         "90th Percentile" = 6) %>%
+  rename("10th Percentile" = 1,
+         "Lower Quartile" = 2,
+         "Median" = 3,
+         "Upper Quartile" = 4,
+         "90th Percentile" = 5) %>%
   kable(format = "html", align = "lrrrrr") %>%
   kable_styling() %>%
   row_spec(0, background = "#407EC9", color = "white")
@@ -173,6 +187,8 @@ y1_lhc_neg_ri_tab_vis <- y1_lhc_neg_ri_tab_formatted %>%
 ### Creating input df
 
 y1_ct_activity_tab <- y1_ct_list[[2]] %>%
+  ungroup() %>%
+  complete(Trial, ct_rebook, fill = list(Total = 0)) %>%
   group_by(ct_rebook) %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -205,6 +221,8 @@ y1_ct_activity_tab_vis <- y1_ct_activity_tab_formatted %>%
 ### Creating input df
 
 y1_ct_scan_outcomes_tab <- y1_ct_list[[3]] %>%
+  ungroup() %>%
+  complete(Trial, ct_outcome, fill = list(Total = 0)) %>%
   group_by(ct_outcome) %>%
   summarize(
     percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
@@ -237,6 +255,8 @@ y1_ct_scan_outcomes_tab_vis <- y1_ct_scan_outcomes_tab_formatted %>%
 ### Creating input df
 
 y1_ct_neg_fu_tab <- y1_ct_list[[8]] %>%
+  ungroup() %>%
+  complete(Trial, ct_neg_fu, fill = list(Total = 0)) %>% ##########
   filter(ct_neg_fu == "FU") %>%
   group_by(ct_neg_fu) %>%
   summarize(
@@ -266,4 +286,37 @@ y1_ct_neg_fu_tab_vis <- y1_ct_neg_fu_tab_formatted %>%
 
 
 # ct scan opt-outs --------------------------------------------------------
+
+y1_ct_opt_out_tab <- y1_ct_list[[2]] %>%
+  ungroup() %>%
+  complete(Trial, ct_rebook, fill = list(Total = 0)) %>%
+  filter(ct_rebook == "Opt-out") %>%
+  group_by(ct_rebook) %>%
+  summarize(
+    percentile_10 = round(quantile(Total, probs = 0.1, na.rm = TRUE), 0),
+    lower_quartile = round(quantile(Total, probs = 0.25, na.rm = TRUE), 0),
+    median = round(median(Total, na.rm = TRUE), 0),
+    upper_quartile = round(quantile(Total, probs = 0.75, na.rm = TRUE), 0),
+    percentile_90 = round(quantile(Total, probs = 0.9, na.rm = TRUE), 0)
+  )
+
+y1_ct_opt_out_tab_formatted <- y1_ct_opt_out_tab %>%
+  mutate_if(is.numeric, ~comma(., big.mark = ","))
+
+### Creating table output
+
+y1_ct_opt_out_tab_vis <- y1_ct_opt_out_tab_formatted %>%
+  select(-1) %>%
+  ungroup() %>%
+  rename("10th Percentile" = 1,
+         "Lower Quartile" = 2,
+         "Median" = 3,
+         "Upper Quartile" = 4,
+         "90th Percentile" = 5) %>%
+  kable(format = "html", align = "lrrrrr") %>%
+  kable_styling() %>%
+  row_spec(0, background = "#407EC9", color = "white")
+
+# init scan outcomes table ------------------------------------------------
+
 
