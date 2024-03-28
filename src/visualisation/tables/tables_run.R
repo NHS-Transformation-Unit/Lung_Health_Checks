72fb6e41f9e8542bbbb69d6d5a95806e20e3f746
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-03-28 13:30:20 +0000

    Completed table scripts for y2-5 and ammended table_run.R script to run all tables in sequence.

--- src/visualisation/tables/tables_run.R
+++ src/visualisation/tables/tables_run.R
@@ -1,3 +1,7 @@
 
 # Run tables files for each year
 source(paste0(here(),"/src/visualisation/tables/y1_tables.R"))
+source(paste0(here(),"/src/visualisation/tables/y2_tables.R"))
+source(paste0(here(),"/src/visualisation/tables/y3_tables.R"))
+source(paste0(here(),"/src/visualisation/tables/y4_tables.R"))
+source(paste0(here(),"/src/visualisation/tables/y5_tables.R"))
