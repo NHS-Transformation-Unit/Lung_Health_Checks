2075c1332ca42875ddb0479af60d40bb6abc7184
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-03 12:56:10 +0100

    Y5 incidental findings additions.

--- src/model/y5/y5_10_m24_treat.R
+++ src/model/y5/y5_10_m24_treat.R
@@ -14,6 +14,7 @@ y5_m24_cancer_df <- y5_m24_treat_groups_list[[6]]
 y5_m24_opt_out <- y5_m24_treat_groups_list[[7]] %>%
   select(1:3)
 y5_m48_FU_input_df <- y5_m24_treat_groups_list[[4]]
+y5_m24_inc_df <- y5_m24_treat_groups_list[[8]]
 
 # Simulate Year 5 24-month Treatment Modalities ----------------------------
 
@@ -26,3 +27,27 @@ y5_m24_modalities_df <- m24_treatment_modalities(input_df = y5_m24_cancer_df,
                                                  chemo_rate = m24_treatment_chemo_y5,
                                                  nadj_imm_rate = m24_treatment_nadj_imm_y5)
 
+
+# Simulate Year 5 24-Month Incidental Findings ----------------------------
+
+y5_m24_incidental_df <- m24_incidental_findings(input_df = y5_m24_inc_df,
+                                                cc_rate = m24_inc_cc_y5, 
+                                                emp_rate = m24_inc_emp_y5, 
+                                                ila_rate = m24_inc_ila_y5, 
+                                                meda_rate = m24_inc_meda_y5, 
+                                                avc_rate = m24_inc_avc_y5, 
+                                                bro_rate = m24_inc_bro_y5, 
+                                                rbr_rate = m24_inc_rbr_y5, 
+                                                taa_rate = m24_inc_taa_y5, 
+                                                ost_rate = m24_inc_ost_y5, 
+                                                lsl_rate = m24_inc_lsl_y5, 
+                                                ren_rate = m24_inc_ren_y5, 
+                                                adr_rate = m24_inc_adr_y5, 
+                                                oca_rate = m24_inc_oca_y5, 
+                                                ple_rate = m24_inc_ple_y5, 
+                                                sbl_rate = m24_inc_sbl_y5, 
+                                                con_rate = m24_inc_con_y5, 
+                                                bon_rate = m24_inc_bon_y5, 
+                                                fra_rate = m24_inc_fra_y5, 
+                                                thy_rate = m24_inc_thy_y5, 
+                                                aaa_rate = m24_inc_aaa_y5)
--- src/model/y5/y5_6_m3_treat.R
+++ src/model/y5/y5_6_m3_treat.R
@@ -11,6 +11,7 @@ y5_m3_treat_groups_list <- m3_treat_groups(input_df = y5_m3_FU_input_df,
 
 y5_m3_add_diags_df <- y5_m3_treat_groups_list[[3]]
 y5_m3_cancer_df <- y5_m3_treat_groups_list[[6]]
+y5_m3_inc_df <- y5_m3_treat_groups_list[[8]]
 
 
 # Simulate Year 5 3-month Treatment Modalities ----------------------------
@@ -30,3 +31,28 @@ y5_m12_FU_input_df <- y5_m3_treat_groups_list[[4]]
 
 y5_m3_opt_out <- y5_m3_treat_groups_list[[7]] %>%
   select(1:3)
+
+
+# Simulate Year 5 3-Month Incidental Findings -----------------------------
+
+y5_m3_incidental_df <- m3_incidental_findings(input_df = y5_m3_inc_df,
+                                              cc_rate = m3_inc_cc_y5, 
+                                              emp_rate = m3_inc_emp_y5, 
+                                              ila_rate = m3_inc_ila_y5, 
+                                              meda_rate = m3_inc_meda_y5, 
+                                              avc_rate = m3_inc_avc_y5, 
+                                              bro_rate = m3_inc_bro_y5, 
+                                              rbr_rate = m3_inc_rbr_y5, 
+                                              taa_rate = m3_inc_taa_y5, 
+                                              ost_rate = m3_inc_ost_y5, 
+                                              lsl_rate = m3_inc_lsl_y5, 
+                                              ren_rate = m3_inc_ren_y5, 
+                                              adr_rate = m3_inc_adr_y5, 
+                                              oca_rate = m3_inc_oca_y5, 
+                                              ple_rate = m3_inc_ple_y5, 
+                                              sbl_rate = m3_inc_sbl_y5, 
+                                              con_rate = m3_inc_con_y5, 
+                                              bon_rate = m3_inc_bon_y5, 
+                                              fra_rate = m3_inc_fra_y5, 
+                                              thy_rate = m3_inc_thy_y5, 
+                                              aaa_rate = m3_inc_aaa_y5)
