149ba07472f318bc2931571541785c42219e6c02
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-03 12:55:43 +0100

    Y3 incidental findings additions.

--- src/model/y3/y3_10_m24_treat.R
+++ src/model/y3/y3_10_m24_treat.R
@@ -14,6 +14,7 @@ y3_m24_cancer_df <- y3_m24_treat_groups_list[[6]]
 y3_m24_opt_out <- y3_m24_treat_groups_list[[7]] %>%
   select(1:3)
 y3_m48_FU_input_df <- y3_m24_treat_groups_list[[4]]
+y3_m24_inc_df <- y3_m24_treat_groups_list[[8]]
 
 # Simulate Year 3 24-month Treatment Modalities ----------------------------
 
@@ -25,3 +26,28 @@ y3_m24_modalities_df <- m24_treatment_modalities(input_df = y3_m24_cancer_df,
                                                  chemrad_rate = m24_treatment_chemrad_y3,
                                                  chemo_rate = m24_treatment_chemo_y3,
                                                  nadj_imm_rate = m24_treatment_nadj_imm_y3)
+
+
+# Simulate Year 3 24-Month Incidental Findings ----------------------------
+
+y3_m24_incidental_df <- m24_incidental_findings(input_df = y3_m24_inc_df,
+                                                cc_rate = m24_inc_cc_y3, 
+                                                emp_rate = m24_inc_emp_y3, 
+                                                ila_rate = m24_inc_ila_y3, 
+                                                meda_rate = m24_inc_meda_y3, 
+                                                avc_rate = m24_inc_avc_y3, 
+                                                bro_rate = m24_inc_bro_y3, 
+                                                rbr_rate = m24_inc_rbr_y3, 
+                                                taa_rate = m24_inc_taa_y3, 
+                                                ost_rate = m24_inc_ost_y3, 
+                                                lsl_rate = m24_inc_lsl_y3, 
+                                                ren_rate = m24_inc_ren_y3, 
+                                                adr_rate = m24_inc_adr_y3, 
+                                                oca_rate = m24_inc_oca_y3, 
+                                                ple_rate = m24_inc_ple_y3, 
+                                                sbl_rate = m24_inc_sbl_y3, 
+                                                con_rate = m24_inc_con_y3, 
+                                                bon_rate = m24_inc_bon_y3, 
+                                                fra_rate = m24_inc_fra_y3, 
+                                                thy_rate = m24_inc_thy_y3, 
+                                                aaa_rate = m24_inc_aaa_y3)
--- src/model/y3/y3_4_init_treat.R
+++ src/model/y3/y3_4_init_treat.R
@@ -9,6 +9,7 @@ y3_init_treat_groups_list <- initial_treat_groups(input_df = y3_ct_output,
 
 y3_init_add_diags_df <- y3_init_treat_groups_list[[2]]
 y3_init_cancer_df <- y3_init_treat_groups_list[[5]]
+y3_init_inc_df <- y3_init_treat_groups_list[[6]]
 
 
 # Simulate Year 3 Initial Treatment Modalities ----------------------------
@@ -23,3 +24,28 @@ y3_init_modalities_df <- initial_treatment_modalities(input_df = y3_init_cancer_
                                                       nadj_imm_rate = initial_treatment_nadj_imm_y3)
 
 y3_m3_FU_input_df <- y3_init_treat_groups_list[[3]]
+
+
+# Simulate Year 3 Initial Incidental Findings -----------------------------
+
+y3_init_incidental_df <- initial_incidental_findings(input_df = y3_init_inc_df,
+                                                     cc_rate = initial_inc_cc_y3, 
+                                                     emp_rate = initial_inc_emp_y3, 
+                                                     ila_rate = initial_inc_ila_y3, 
+                                                     meda_rate = initial_inc_meda_y3, 
+                                                     avc_rate = initial_inc_avc_y3, 
+                                                     bro_rate = initial_inc_bro_y3, 
+                                                     rbr_rate = initial_inc_rbr_y3, 
+                                                     taa_rate = initial_inc_taa_y3, 
+                                                     ost_rate = initial_inc_ost_y3, 
+                                                     lsl_rate = initial_inc_lsl_y3, 
+                                                     ren_rate = initial_inc_ren_y3, 
+                                                     adr_rate = initial_inc_adr_y3, 
+                                                     oca_rate = initial_inc_oca_y3, 
+                                                     ple_rate = initial_inc_ple_y3, 
+                                                     sbl_rate = initial_inc_sbl_y3, 
+                                                     con_rate = initial_inc_con_y3, 
+                                                     bon_rate = initial_inc_bon_y3, 
+                                                     fra_rate = initial_inc_fra_y3, 
+                                                     thy_rate = initial_inc_thy_y3, 
+                                                     aaa_rate = initial_inc_aaa_y3)
