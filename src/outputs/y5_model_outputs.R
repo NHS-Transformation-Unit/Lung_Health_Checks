2b724e14ead68747d0adca3ef20c43ee98a5e5a2
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-03 12:56:39 +0100

    Changes to model output scripts to allow for inclusion of incidental findings.

--- src/outputs/y5_model_outputs.R
+++ src/outputs/y5_model_outputs.R
@@ -17,24 +17,28 @@ y5_init_FU_op <- y5_init_treat_groups_list[[1]]
 y5_init_diag_mod_op <- y5_init_diags_modalities_df
 y5_init_diag_outcome_op <-y5_init_treat_groups_list[[4]]
 y5_init_treat_mod_op <- y5_init_modalities_df
+y5_init_inc_op <- y5_init_incidental_df
 
 y5_m3_book_op <- y5_m3_treat_groups_list[[1]]
 y5_m3_FU_op <- y5_m3_treat_groups_list[[2]]
 y5_m3_diag_mod_op <- y5_m3_diags_modalities_df
 y5_m3_diag_outcome_op <- y5_m3_treat_groups_list[[5]]
 y5_m3_treat_mod_op <- y5_m3_modalities_df
+y5_m3_inc_op <- y5_m3_incidental_df
 
 y5_m12_book_op <- y5_m12_treat_groups_list[[1]]
 y5_m12_FU_op <- y5_m12_treat_groups_list[[2]]
 y5_m12_diag_mod_op <- y5_m12_diags_modalities_df
 y5_m12_diag_outcome_op <- y5_m12_treat_groups_list[[5]]
 y5_m12_treat_mod_op <- y5_m12_modalities_df
+y5_m12_inc_op <- y5_m12_incidental_df
 
 y5_m24_book_op <- y5_m24_treat_groups_list[[1]]
 y5_m24_FU_op <- y5_m24_treat_groups_list[[2]]
 y5_m24_diag_mod_op <- y5_m24_diags_modalities_df
 y5_m24_diag_outcome_op <- y5_m24_treat_groups_list[[5]]
 y5_m24_treat_mod_op <- y5_m24_modalities_df
+y5_m24_inc_op <- y5_m24_incidental_df
 
 ## Exporting defined objects using output function
 
@@ -52,21 +56,25 @@ excel_output(
     y5_init_diag_mod_op = y5_init_diag_mod_op,
     y5_init_diag_outcome_op = y5_init_diag_outcome_op,
     y5_init_treat_mod_op = y5_init_treat_mod_op,
+    y5_init_inc_op = y5_init_inc_op,
     y5_m3_book_op = y5_m3_book_op,
     y5_m3_FU_op = y5_m3_FU_op,
     y5_m3_diag_mod_op = y5_m3_diag_mod_op,
     y5_m3_diag_outcome_op = y5_m3_diag_outcome_op,
     y5_m3_treat_mod_op = y5_m3_treat_mod_op,
+    y5_m3_inc_op = y5_m3_inc_op,
     y5_m12_book_op = y5_m12_book_op,
     y5_m12_FU_op = y5_m12_FU_op,
     y5_m12_diag_mod_op = y5_m12_diag_mod_op,
     y5_m12_diag_outcome_op = y5_m12_diag_outcome_op,
     y5_m12_treat_mod_op = y5_m12_treat_mod_op,
+    y5_m12_inc_op = y5_m12_inc_op,
     y5_m24_book_op = y5_m24_book_op,
     y5_m24_FU_op = y5_m24_FU_op,
     y5_m24_diag_mod_op = y5_m24_diag_mod_op,
     y5_m24_diag_outcome_op = y5_m24_diag_outcome_op,
-    y5_m24_treat_mod_op = y5_m24_treat_mod_op
+    y5_m24_treat_mod_op = y5_m24_treat_mod_op,
+    y5_m24_inc_op = y5_m24_inc_op
   ),
   overwrite = TRUE
 )
