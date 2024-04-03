ae38e7535a885844b918939a0d7ffec75c2ee2b6
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-03 12:55:56 +0100

    Y4 incidental findings additions.

--- src/model/y4/y4_10_m24_treat.R
+++ src/model/y4/y4_10_m24_treat.R
@@ -14,6 +14,7 @@ y4_m24_cancer_df <- y4_m24_treat_groups_list[[6]]
 y4_m24_opt_out <- y4_m24_treat_groups_list[[7]] %>%
   select(1:3)
 y4_m48_FU_input_df <- y4_m24_treat_groups_list[[4]]
+y4_m24_inc_df <- y3_m24_treat_groups_list[[8]]
 
 # Simulate Year 4 24-month Treatment Modalities ----------------------------
 
@@ -25,3 +26,28 @@ y4_m24_modalities_df <- m24_treatment_modalities(input_df = y4_m24_cancer_df,
                                                  chemrad_rate = m24_treatment_chemrad_y4,
                                                  chemo_rate = m24_treatment_chemo_y4,
                                                  nadj_imm_rate = m24_treatment_nadj_imm_y4)
+
+
+# Simulate Year 4 24-Month Incidental Findings ----------------------------
+
+y4_m24_incidental_df <- m24_incidental_findings(input_df = y4_m24_inc_df,
+                                                cc_rate = m24_inc_cc_y4, 
+                                                emp_rate = m24_inc_emp_y4, 
+                                                ila_rate = m24_inc_ila_y4, 
+                                                meda_rate = m24_inc_meda_y4, 
+                                                avc_rate = m24_inc_avc_y4, 
+                                                bro_rate = m24_inc_bro_y4, 
+                                                rbr_rate = m24_inc_rbr_y4, 
+                                                taa_rate = m24_inc_taa_y4, 
+                                                ost_rate = m24_inc_ost_y4, 
+                                                lsl_rate = m24_inc_lsl_y4, 
+                                                ren_rate = m24_inc_ren_y4, 
+                                                adr_rate = m24_inc_adr_y4, 
+                                                oca_rate = m24_inc_oca_y4, 
+                                                ple_rate = m24_inc_ple_y4, 
+                                                sbl_rate = m24_inc_sbl_y4, 
+                                                con_rate = m24_inc_con_y4, 
+                                                bon_rate = m24_inc_bon_y4, 
+                                                fra_rate = m24_inc_fra_y4, 
+                                                thy_rate = m24_inc_thy_y4, 
+                                                aaa_rate = m24_inc_aaa_y4)
--- src/model/y4/y4_8_m12_treat.R
+++ src/model/y4/y4_8_m12_treat.R
@@ -11,6 +11,7 @@ y4_m12_treat_groups_list <- m12_treat_groups(input_df = y4_m12_FU_input_df,
 
 y4_m12_add_diags_df <- y4_m12_treat_groups_list[[3]]
 y4_m12_cancer_df <- y4_m12_treat_groups_list[[6]]
+y4_m12_inc_df <- y4_m12_treat_groups_list[[8]]
 
 
 # Simulate Year 4 12-month Treatment Modalities ----------------------------
@@ -30,3 +31,28 @@ y4_m24_FU_input_df <- y4_m12_treat_groups_list[[4]]
 
 y4_m12_opt_out <- y4_m12_treat_groups_list[[7]] %>%
   select(1:3)
+
+
+# Simulate Year 4 12-Month Incidental Findings ----------------------------
+
+y4_m12_incidental_df <- m12_incidental_findings(input_df = y4_m12_inc_df,
+                                                cc_rate = m12_inc_cc_y4, 
+                                                emp_rate = m12_inc_emp_y4, 
+                                                ila_rate = m12_inc_ila_y4, 
+                                                meda_rate = m12_inc_meda_y4, 
+                                                avc_rate = m12_inc_avc_y4, 
+                                                bro_rate = m12_inc_bro_y4, 
+                                                rbr_rate = m12_inc_rbr_y4, 
+                                                taa_rate = m12_inc_taa_y4, 
+                                                ost_rate = m12_inc_ost_y4, 
+                                                lsl_rate = m12_inc_lsl_y4, 
+                                                ren_rate = m12_inc_ren_y4, 
+                                                adr_rate = m12_inc_adr_y4, 
+                                                oca_rate = m12_inc_oca_y4, 
+                                                ple_rate = m12_inc_ple_y4, 
+                                                sbl_rate = m12_inc_sbl_y4, 
+                                                con_rate = m12_inc_con_y4, 
+                                                bon_rate = m12_inc_bon_y4, 
+                                                fra_rate = m12_inc_fra_y4, 
+                                                thy_rate = m12_inc_thy_y4, 
+                                                aaa_rate = m12_inc_aaa_y4)
