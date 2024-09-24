c6f68f22e0c9af54e6dae14794b51ab4289353be
ASW-Analyst <andy.wilson8@nhs.net>
2024-09-24 10:07:41 +0100

    Fix chart title

--- src/visualisation/functions/ct.R
+++ src/visualisation/functions/ct.R
@@ -107,7 +107,7 @@ ct_ex_chart_visual <- function(input_df, fill_hex, sub_label){
     geom_histogram(fill = fill_hex, alpha = 0.5, color = "#000000", bins = 20) +
     labs(x = "Number of patients",
          y = "Number of Trials",
-         title = "Histogram of in year patients opting out at CT scan",
+         title = "Histogram of in year CT scan exclusions",
          subtitle = sub_label,
          caption = "Model Output") +
     theme_tu_white(hex_col = "#407EC9")
