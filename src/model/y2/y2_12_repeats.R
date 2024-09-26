1af83009b70b2f00ac132b0fcd011bd8e1c8ef0e
ASW-Analyst <andy.wilson8@nhs.net>
2024-09-26 19:38:15 +0100

    Update repeats to apply re-invitation probabilities to CT DNAs and LHC DNAs and opt-outs. Applied across each year.

--- src/model/y2/y2_12_repeats.R
+++ src/model/y2/y2_12_repeats.R
@@ -2,10 +2,18 @@
 # Create Y4 Re-invites ----------------------------------------------------
 
 y2_lhc_opt_out_sum <- y2_lhc_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < lhc_oodna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
 y2_lhc_dna_opt_out_sum <- y2_lhc_dna_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < lhc_oodna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -14,10 +22,18 @@ y2_lhc_neg_ri_sum <- y2_lhc_neg_ri %>%
   summarise(Total = n())
 
 y2_ct_opt_out_sum <- y2_ct_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
 y2_m3_opt_out_sum <- y2_m3_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -36,6 +52,10 @@ y4_reinvites <- rbind(y2_lhc_opt_out_sum,
 # Create Y5 Re-invites ----------------------------------------------------
 
 y2_m12_opt_out_sum <- y2_m12_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -45,6 +65,10 @@ y5_reinvites_y2 <- y2_m12_opt_out_sum
 # Create Y6 Re-invites ----------------------------------------------------
 
 y2_m24_opt_out_sum <- y2_m24_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y2 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
--- src/model/y4/y4_12_repeats.R
+++ src/model/y4/y4_12_repeats.R
@@ -2,10 +2,18 @@
 # Create Y6 Re-invites ----------------------------------------------------
 
 y4_lhc_opt_out_sum <- y4_lhc_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < lhc_oodna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
 y4_lhc_dna_opt_out_sum <- y4_lhc_dna_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < lhc_oodna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -14,10 +22,18 @@ y4_lhc_neg_ri_sum <- y4_lhc_neg_ri %>%
   summarise(Total = n())
 
 y4_ct_opt_out_sum <- y4_ct_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
 y4_m3_opt_out_sum <- y4_m3_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -37,6 +53,10 @@ y4_reinvites <- rbind(y4_lhc_opt_out_sum,
 # Create Y7 Re-invites ----------------------------------------------------
 
 y4_m12_opt_out_sum <- y4_m12_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
@@ -46,6 +66,10 @@ y7_reinvites_y4 <- y4_m12_opt_out_sum
 # Create Y8 Re-invites ----------------------------------------------------
 
 y4_m24_opt_out_sum <- y4_m24_opt_out %>%
+  mutate(reinvite_prob = runif(n()),
+         reinvite = case_when(reinvite_prob < ct_dna_ri_rate_y4 ~ 1,
+                              TRUE ~ 0)) %>%
+  filter(reinvite == 1) %>%
   group_by(Trial) %>%
   summarise(Total = n())
 
