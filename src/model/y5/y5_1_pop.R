4ff7397d8d855de6a49de3c44fe98d0fc9408fc6
ASW-Analyst <andy.wilson8@nhs.net>
2024-09-26 16:44:51 +0100

    Adjust re-invites to be cumulatively to account for cumulative previously screened in Year 4 and Year 5.

--- src/model/y5/y5_1_pop.R
+++ src/model/y5/y5_1_pop.R
@@ -6,7 +6,7 @@ y5_uptake_pop <- uptake_pop(start_pop = total_pop,
                             smk_current = smk_rate_current_y5,
                             smk_prev = smk_rate_prev_y5,
                             screened_prev = y5_screened_input_df,
-                            repeats = y5_reinvites,
+                            repeats = (y3_reinvites + y4_reinvites + y5_reinvites),
                             uptake = uptake_rate_y5)
 
 # Create previously screened dataframe ------------------------------------
