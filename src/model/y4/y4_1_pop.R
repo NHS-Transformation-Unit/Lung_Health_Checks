9710889b9f2b274d119ff74661ef3675e27dd980
ASW-Analyst <andy.wilson8@nhs.net>
2024-10-01 14:07:22 +0100

    Amend reinvitation dataframe to join back to total trial numbers and coalesce to account for 0 invites for missing trials.

--- src/model/y4/y4_1_pop.R
+++ src/model/y4/y4_1_pop.R
@@ -1,4 +1,18 @@
 
+# Update reinvites to account for any missing trials ----------------------
+
+y4_reinvites_update <-data.frame(
+                                  Trial = seq(trials),
+                                  Total = 0
+                                ) %>%
+  left_join(y4_reinvites, by = c("Trial")) %>%
+  mutate(Total = coalesce(Total.y, Total.x)) %>%
+  select(c(1,4))
+
+y4_reinvites_update_cumul <- union_all(y3_reinvites_update, y4_reinvites_update) %>%
+  group_by(Trial) %>%
+  summarise(Total = sum(Total, na.rm = TRUE))
+
 # Simulate Year 4 Uptake Population ---------------------------------------
 
 y4_uptake_pop <- uptake_pop(start_pop = total_pop,
@@ -6,7 +20,7 @@ y4_uptake_pop <- uptake_pop(start_pop = total_pop,
                             smk_current = smk_rate_current_y4,
                             smk_prev = smk_rate_prev_y4,
                             screened_prev = y4_screened_input_df,
-                            repeats = (y3_reinvites + y4_reinvites),
+                            repeats = y4_reinvites_update_cumul,
                             uptake = uptake_rate_y4)
 
 # Created previously screened dataframe -----------------------------------
